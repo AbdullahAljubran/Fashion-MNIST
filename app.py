74a2b480c2d65e01711cfec4b16ce865be517665
Hussah Almuzaini <67816015+hussah-almuzaini@users.noreply.github.com>
2025-05-22 00:37:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def main():
         st.metric("Predicted Category", class_names[pred_label] if pred_label != -1 else "Unknown")
 
 if __name__ == '__main__':
-    main() هذا كود ستريم ليت ابغى نفس التنسيق
+    main() 
