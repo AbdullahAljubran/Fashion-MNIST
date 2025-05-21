906e139e9013eb3f101caeb12c2a08335e124408
Hussah Almuzaini <67816015+hussah-almuzaini@users.noreply.github.com>
2025-05-21 20:54:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import numpy as np
 import cv2
 import joblib
 import base64
-from io import BytesIO
-from PIL import Image
 
 st.set_page_config(page_title="Image Clustering", layout="centered")
 
@@ -52,19 +50,14 @@ st.markdown("""
             color: #4e3620;
             text-align: center;
         }
-
-   
-  
     </style>
 """, unsafe_allow_html=True)
 
-
-# load models 
 pca_model = joblib.load("pca_model.pkl")
-kmeans_model = joblib.load("kmeans_model10.pkl")
+kmeans_model = joblib.load("kmeans_model.pkl")
 dbscan_model = joblib.load("dbscan_model.pkl")
 
-st.title("Image Clustering using PCA , kmeans and DBSCAN ")
+st.title("Image Clustering using PCA, KMeans, and DBSCAN")
 
 uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
@@ -72,26 +65,19 @@ if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-    
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, bw = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
     resized = cv2.resize(bw, (28, 28))
     flat = resized.flatten() / 255.0
     flat = flat.reshape(1, -1)
 
-    #PCA 
     pca_features = pca_model.transform(flat)
-    #kmeans 
     kmeans_cluster = kmeans_model.predict(pca_features)[0]
-    #dbscan
     dbscan_cluster = dbscan_model.fit_predict(pca_features)[0]
 
-    #base64
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    pil_img = Image.fromarray(img_rgb)
-    buffered = BytesIO()
-    pil_img.save(buffered, format="PNG")
-    img_base64 = base64.b64encode(buffered.getvalue()).decode()
+    _, img_encoded = cv2.imencode('.png', img)
+    img_base64 = base64.b64encode(img_encoded).decode()
 
     st.markdown(f"""
         <div class="image-frame">
@@ -100,7 +86,6 @@ if uploaded_file is not None:
     """, unsafe_allow_html=True)
 
     dbscan_text = f"Cluster: {dbscan_cluster}" if dbscan_cluster != -1 else "Outlier (No Cluster)"
-
     st.markdown(f"""
         <div class="results-container">
             <div class="result-box">
